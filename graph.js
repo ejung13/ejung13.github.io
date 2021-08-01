4df8c7319ff03bee42dc962cdf7d0deae68cb792
ejung13 <88182085+ejung13@users.noreply.github.com>
2021-08-01 14:04:33 +0900

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,35 +1,37 @@
+<script>
+
 // set the dimensions and margins of the graph
-const margin = {top: 40, right: 150, bottom: 60, left: 30};
-const width = 800 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+var margin = {top: 40, right: 150, bottom: 60, left: 30},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-const xScale = d3.scaleLinear().range([0, width]);
-const yScale = d3.scaleLinear().rangeRound([height, 0]);
+var xScale = d3.scaleLinear().range([0, width]);
+var yScale = d3.scaleLinear().rangeRound([height, 0]);
 
-const yaxis = d3.axisLeft().scale(yScale);
-const xaxis = d3.axisBottom().scale(xScale).tickFormat(d3.format("d"));
+var yaxis = d3.axisLeft().scale(yScale);
+var xaxis = d3.axisBottom().scale(xScale).tickFormat(d3.format("d"));
 
 // gridlines in x/y axis function
 
 function make_x_gridlines() {
     return d3.axisBottom(xScale)
-    .ticks(5);
+    .ticks(5)
 }
 
 function make_y_gridlines() {
     return d3.axisLeft(yScale)
-    .ticks(5);
+    .ticks(5)
 }
 
 
 // append the svg object to the body of the page
-
-const svg = d3.select("#scatter_plot").append("svg")
-    .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-    .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
-
-const graph = svg.append("g")  
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#scatter_plot")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
 d3.csv("https://raw.githubusercontent.com/ejung13/ejung13.github.io/main/suicide_2000.csv", function(data) {
@@ -58,6 +60,7 @@ d3.csv("https://raw.githubusercontent.com/ejung13/ejung13.github.io/main/suicide
         .tickFormat("")
       );
 
+
     svg.append("g")
       .attr("class", "grid")
       .call(make_y_gridlines()
@@ -85,36 +88,38 @@ d3.csv("https://raw.githubusercontent.com/ejung13/ejung13.github.io/main/suicide
     .domain([500, 25000, 55000])
     .range([ "green", "gold"]);
 
+
   // -1- Create a tooltip div that is hidden by default:
   var tooltip = d3.select("#scatter_plot")
-    .append("div")
+    .append("svg")
       .style("opacity", 0)
       .attr("class", "tooltip")
       .style("background-color", "black")
       .style("border-radius", "5px")
       .style("padding", "10px")
-      .style("color", "white");
+      .style("color", "white")
 
   // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
   var showTooltip = function(d) {
     tooltip
       .transition()
       .duration(200)
+    tooltip
       .style("opacity", 1)
       .html("Country: " + d.country)
       .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px");
+      .style("top", (d3.mouse(this)[1]+30) + "px")
   }
   var moveTooltip = function(d) {
     tooltip
       .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px");
+      .style("top", (d3.mouse(this)[1]+30) + "px")
   }
   var hideTooltip = function(d) {
     tooltip
       .transition()
       .duration(200)
-      .style("opacity", 0);
+      .style("opacity", 0)
   }
   
   // Add dots
@@ -122,17 +127,17 @@ d3.csv("https://raw.githubusercontent.com/ejung13/ejung13.github.io/main/suicide
     .selectAll("dot")
     .data(data)
     .enter()
-    .append("circle")  
-    .attr("cx", function (d) { return x(d.gdp_per_capita); } )
-    .attr("cy", function (d) { return y(d.suicides_100k_pop); } )
-    .attr("r", 5)
-    .style("fill", function (d) { return color(d.gdp_per_capita) } )
+    .append("circle")
+      .attr("cx", function (d) { return x(d.gdp_per_capita); } )
+      .attr("cy", function (d) { return y(d.suicides_100k_pop); } )
+      .attr("r", 5)
+      .style("fill", function (d) { return color(d.gdp_per_capita) } )
       // -3- Trigger the functions for hover
     .on("mouseover", showTooltip )
     .on("mousemove", moveTooltip )
-    .on("mouseleave", hideTooltip );
+    .on("mouseleave", hideTooltip )
     
-   
+    
 // Features of the annotation
 const annotations = [
   {
@@ -149,9 +154,12 @@ const annotations = [
 
 // Add annotation to the chart
 const makeAnnotations = d3.annotation()
-  .annotations(annotations);
+  .annotations(annotations)
 d3.select("#scatter_plot")
   .append("g")
-  .call(makeAnnotations);
+  .call(makeAnnotations)
 
 })
+
+
+</script>
